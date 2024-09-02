d5e5bff3ee8fba1e892149303083ac8e0a46af10
ali <alimosa2062@gmail.com>
2024-09-02 14:10:05 +0300

    final

--- main/models.py
+++ main/models.py
@@ -4,6 +4,7 @@ class Compares(models.Model):
     username1 = models.CharField(max_length=22)
     username2 = models.CharField(max_length=22)
     percentage = models.IntegerField()
+    co_time = models.DateTimeField(auto_now=True)
     
 
 # Create your models here.
--- main/views.py
+++ main/views.py
@@ -41,14 +41,19 @@ def find_watchlist(username):
     ssd = []
     ssk = []
     length = 0
+    table = []
+    titles_location = ""
+    titles_prep = []
+    titles = []
     if soup.find('div', class_ = 'pagination'):
         ssd = soup.find('div', class_ = 'pagination').find_all('a')
         ssk = [str(i) for i in ssd]
         length = int(ssk[-1].split('>')[1].split('<')[0])
-    table = soup.find('ul', class_ = 'poster-list -p125 -grid -scaled128')
-    titles_location = table.find_all('img')
-    titles_prep = [str(title).split('''"''') for title in titles_location]
-    titles = [i[1] for i in titles_prep]
+    if soup.find('ul', class_ = 'poster-list -p125 -grid -scaled128'):
+        table = soup.find('ul', class_ = 'poster-list -p125 -grid -scaled128')
+        titles_location = table.find_all('img')
+        titles_prep = [str(title).split('''"''') for title in titles_location]
+        titles = [i[1] for i in titles_prep]
     
     if len(ssd) > 0:
             for i in range(2, length):
@@ -67,25 +72,31 @@ def compare(username1, username2):
     list2 = find_movies(username2)
     watchlist1 = find_watchlist(username1)
     watchlist2 = find_watchlist(username2)
+    watchlist = watchlist1 + watchlist2
+    recommendation = ""
+    percentage = 0
+    intersection2 =[]
     
     intersection = set(list1).intersection(list2)
     movies_mean = (len(set(list1))+len(set(list2)))/2
     percentage1= round((len(intersection) / movies_mean )*100)
-    
-    intersection2 = list(set(watchlist1).intersection(watchlist2))
-    watchlist_min = min(len(watchlist1), len(watchlist2))
-    percentage2 = round((len(intersection2) / watchlist_min )*100)
-    recommendation = ""
-    if intersection2 != []:
-        recommendation = random.choice(intersection2)
+    if len(watchlist1)>0 and len(watchlist2)> 0:
+        intersection2 = list(set(watchlist1).intersection(watchlist2))
+        watchlist_min = min(len(watchlist1), len(watchlist2))
+        percentage2 = round((len(intersection2) / watchlist_min )*100)
+        percentage = percentage1*0.65 + percentage2*0.35
+        if intersection2 == []:
+            recommendation = random.choice(watchlist)
+        elif intersection2 != []:
+            recommendation = random.choice(intersection2)
     else:
-        recommendation = random.choice(list(list1))
+        percentage = percentage1    
     
-    percentage = percentage1*0.65 + percentage2*0.35
     return percentage, recommendation
 
 def web(request):
     return render(request, 'index.html')
+
 def result(request):
     data = request.POST
     username1 = data['username1']
@@ -95,7 +106,7 @@ def result(request):
         comparation = Compares.objects.get(username1 = username1, username2=username2, percentage=percentage)
     except:
         Compares.objects.create(username1= username1, username2= username2, percentage= percentage)
-    obb = get_object_or_404(Compares, username1=username1, username2=username2)
+    obb = Compares.objects.filter(username1=username1, username2=username2).latest('co_time')
     return render(request, 'result.html', {'obb': obb,
                                            'recommendation': recommendation})
 # Create your views here.
